fe2edd72b8110a20282023930563256322d5260f
Keagen <keegenjt@gmail.com>
2024-01-31 22:12:13 -0500

    Added initial setup/dir for Lab4

---
+++ Lab4/cifar10.py
@@ -0,0 +1,10 @@
+import torch
+import torchvision
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import matplotlib.pyplot as plt
+import numpy as np
+import multiprocessing
+

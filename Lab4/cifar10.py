13a5eb3188d516bcae346ddbb2bb152adf993636
Keagen <keegenjt@gmail.com>
2024-02-05 22:09:26 -0500

    Added sample code to print all class percentages

--- Lab4/cifar10.py
+++ Lab4/cifar10.py
@@ -133,6 +133,24 @@ def main():
             correct += (predicted == labels).sum().item();
     print(f"Accuracy of the network on the 10000 test images: {100 * correct // total} %");
 
+    correct_pred = {classname: 0 for classname in classes}
+    total_pred = {classname: 0 for classname in classes}
+
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predictions = torch.max(outputs, 1)
+
+            for label, prediction in zip(labels, predictions):
+                if label == prediction:
+                    correct_pred[classes[label]] += 1
+                total_pred[classes[label]] += 1
+
+    for classname, correct_count in correct_pred.items():
+        accuracy = 100 * float(correct_count) / total_pred[classname]
+        print(f'Accuracy for class: {classname:5s} is {accuracy:.1f} %')
+
 ########################################
 # Script run protection
 ########################################

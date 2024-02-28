cee7b8f69b3528015b59228cf85cad98eaa1ed5e
Keagen <keegenjt@gmail.com>
2024-02-28 11:24:26 -0500

    Finished by reformatting data and loading into image

--- pca-lab/pca.py
+++ pca-lab/pca.py
@@ -1,5 +1,6 @@
 from PIL import Image;
 import numpy as np;
+import sys;
 
 # Grayscale
 img = Image.open("/home/keag/Github/machine-learning/pca-lab/input-image.bmp");
@@ -15,8 +16,7 @@ img_covar = np.cov(img_norm, rowvar=False)
 # Determine the eigenvectors/values from the covariance 
 eig_val, eig_vec = np.linalg.eigh(img_covar);
 # TODO Pairs might be messed up, may need to transpose eig_vec?
-# who tf knows though?
-eig_pairs = list(zip(eig_val, eig_vec)); 
+eig_pairs = list(zip(eig_val, np.transpose(eig_vec))); 
 
 # Sort and keep the 15 most important eigenvectors
 eig_pairs_sorted = sorted(eig_pairs, key=lambda pair: pair[0], reverse=True);
@@ -27,12 +27,14 @@ total_eigen_sum = np.sum(eig_val);
 key_eigen_sum = np.sum([pair[0] for pair in key_eig_pairs]);
 variance_percent = key_eigen_sum / total_eigen_sum;
 
-# TODO I messed this up
-# Either need to keep original format of linalg.eigh,
-# or alter it back here for this to work
+# Turn key vectors into numpy array
+key_eig_vectors = np.array([pair[1] for pair in key_eig_pairs])
+key_eig_vectors_transposed = np.transpose(key_eig_vectors)
 
-"""
-compressed_image = np.matmul(img_norm, key_eig_pairs);
-lossy_compressed_image = np.matmul(compressed_image, np.transpose(key_eig_pairs)) + img_mean;
-"""
+# Compress image with key vectors
+compressed_image = np.matmul(img_norm, key_eig_vectors_transposed);
+lossy_compressed_image = np.matmul(compressed_image, key_eig_vectors) + img_mean
 
+# Display image
+compressed_image_pil = Image.fromarray(lossy_compressed_image)
+compressed_image_pil.show()
